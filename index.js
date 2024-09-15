9b06ef0e90f36fbff69bdc1deab987d22d0c866b
moureenmusimbi <musimbimoureen4@gmail.com>
2024-09-15 23:30:46 +0300

    commit message

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.add('active'); // Show the menu
+});
+
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove('active'); // Hide the menu
+});
